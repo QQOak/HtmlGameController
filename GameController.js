185f5f4eb7b92e1549a189bf67ececb4be19272e
Mark Oakley <maoakley@qinetiq.com>
2016-02-14 14:41:16 +0000

    Added client side script
    Initial commit of all of all of the client-side HTML, CSS and
    Javascript. C# Website and Python Buggy Code are being placed into
    Separate repositories

---
+++ GameController.js
@@ -0,0 +1,172 @@
+﻿"use strict";
+
+(function ($) {
+
+    $.fn.GameController = function (options) {
+
+        // Credit:
+        // http://stackoverflow.com/questions/18185956/calling-a-function-inside-a-jquery-plugin-from-outside
+
+        //settings parameter will our private parameter to our function
+        //'myplugin', using jQuery.extend append 'options' to our settings
+        var settings = jQuery.extend({
+            id: 'gameController1',
+            outputLabel: null,
+        }, options);
+
+        //Define a reference to our function myplugin which it's 
+        //part of jQuery namespace functions, so we can use later
+        //within inside functions
+        var $jquery = this;
+
+        // Keep track of if we're currently awaiting a response from the server following an ajax Request
+        var currentAction = false;
+        var values = new Array();
+
+
+        var controllerValues = new Object();
+        controllerValues.id = settings.id;
+        //controllerValues.timestamp = Date();
+
+        controllerValues.thumbstickValues = new Array();
+        controllerValues.digitalButtonValues = new Array();
+
+
+        //Define an output object that will work as a reference
+        //for our function
+        var output = {
+
+
+            'setValueByObject': function (obj) {
+                return output;
+            },
+
+
+            'displayOnPage': function(msg) {
+               //settings.outputLabel.html(msg);
+            },
+
+
+            //
+            'setThumbStick': function (id, xAxis, yAxis, direction, magnitude) {
+
+                for (var i = 1; i <= controllerValues.thumbstickValues.length; i++) {
+
+                    // Find and remove an item with this ID
+                    if (controllerValues.thumbstickValues[i - 1].id == id) {
+                        controllerValues.thumbstickValues.splice(i - 1, 1);
+                        break;
+                    }
+                }
+
+                controllerValues.thumbstickValues[controllerValues.thumbstickValues.length] = {
+                    id: id,
+                    xAxis: xAxis,
+                    yAxis: yAxis,
+                    direction: direction,
+                    magnitude: magnitude
+                }
+
+                return output;
+            },
+
+
+            'setDigitalButton': function (id, state) {
+
+                return output;
+            },
+
+
+            //Setup our plugin functions as an object elements
+            /*
+            'setValue': function (elementId, attrib, value) {
+
+                for (var i=1; i <= values.length; i++)
+                {
+                    // see if the we already have an element with this name and 
+                    if ((values[i-1].elementId == elementId)
+                        && (values[i-1].attribute == attrib))
+                    {
+                        //alert('removing:' + elementId + ':' + attrib)
+                        values.splice(i - 1, 1);
+                    }
+                }
+
+                // now add
+                //alert('adding');
+                values[values.length] = {
+                    elementId: elementId,
+                    attribute: attrib,
+                    value: value
+                }
+
+                var msg = '';
+                for (var i = 0; i < values.length; i++)
+                {
+                    var obj = values[i]
+                    msg += '<div style="padding: 20px;">';
+                    msg += '<div>ElementId: ' + obj.elementId + '</div>';
+                    msg += '<div>Attribute: ' + obj.attribute + '</div>';
+                    msg += '<div>Value: ' + obj.value + '</div>';
+                    msg += '</div>';
+                }
+
+                output.displayOnPage(msg);
+
+
+
+                // Enable chaining
+                return output;
+            },
+            */
+
+            'CsDate': function (csDate) {
+                return new Date(parseInt(csDate.substr(6)));
+            },
+
+
+            'updateServer': function () {
+
+                if (!currentAction) {
+                    output.UpdateServer2(JSON.stringify(controllerValues));
+                };
+            },
+
+            'UpdateServer2': function (JSONData) {
+
+                
+                currentAction = true;
+
+                // Display What We're sending to the server
+                //alert(JSON.stringify(controllerValues));
+
+                $.ajax({
+                    type: 'POST',
+                    url: 'Gamepad.ashx',
+                    contentType: "Application/json; charset=utf-8",
+                    dataType: "json",
+                    data: JSONData,
+                    success: function (data) {
+
+                        //settings.outputLabel.html("<div>timestamp: " + output.CsDate(data.timestamp) + "</div><div>id: " + data.id + "</div>");
+                        settings.outputLabel.html("<div>"+JSON.stringify(data)+"</div>");
+                        currentAction = false;
+                    },
+                    error: function (jqXHR, textStatus, errorThrown) {
+                        alert('Failed\n\n' + textStatus + '\n\n' + errorThrown);
+                    }
+                });
+
+
+                return output;
+            },
+
+        };
+
+        //And the final critical step, return our object output to the plugin
+        return output;
+
+    };
+    //Pass the jQuery class so we can use it inside our plugin 'class'
+
+})(jQuery);
---
+++ ThumbStick.js
@@ -0,0 +1,360 @@
+﻿"use strict";
+
+(function ($) {
+
+    $.fn.ThumbStick = function (options) {
+        return this.each(function () {
+
+            var settings = $.extend({
+                gameController: null,
+                stickRadius: 40,
+                initialRadius: 40,
+                maxRadius: 200,
+                openDuration: 400,
+                closeDuration: 100,
+                outputLabel: null
+            }, options);
+
+            // We'll be need to reference this later
+            var $this = this;
+            var centerPosition = getCenter(this);
+
+
+            /// Gets the Central coordinates for an element.
+            function getCenter(elem) {
+                var $elem = $(elem);
+                var pos = $elem.offset();
+
+                pos.left += ($elem.width() / 2);
+                pos.top += ($elem.height() / 2);
+
+                // Make integer following division
+                pos.left = Math.floor(pos.left);
+                pos.top = Math.floor(pos.top);
+                return pos;
+            };
+
+
+
+            var stick = null;
+            var range = null;
+            var touchId = null;
+
+            // Calculate the minimum and maximum x and Y coordinates for the 
+            // This does not account for the limits imposed by keeping the stick contrained with the 
+
+            stick = createStick(this, centerPosition, settings.stickRadius);
+            createRange(this, centerPosition, settings.maxRadius);
+
+            
+
+
+
+
+            $(this).bind('touchstart', function (e) {
+                e.preventDefault();
+                var touches = e.originalEvent.touches || e.originalEvent.changedTouches;
+
+                // The highest index touch event appears to be the latest.
+                var touch = touches[touches.length - 1];
+                touchId = touch.identifier;
+                
+                var pos = getStickPosition(touch);
+                moveStick(stick, pos.pageCoordinates);
+                updatePositionLabel(pos);
+            });
+
+
+            $(this).bind('touchmove', function (e) {
+                e.preventDefault();
+
+                var touches = e.originalEvent.touches || e.originalEvent.changedTouches;
+                var touchCount = touches.length;
+
+
+                for (var i = 0; i <= touchCount - 1; i++) {
+                    var touch = touches[i];
+                    if (touch.identifier == touchId) {
+
+                        var pos = getStickPosition(touch);
+                        moveStick(stick, pos.pageCoordinates);
+                        updatePositionLabel(pos);
+                        break;
+
+                    }
+                }
+
+            });
+
+
+
+
+            $(this).bind('touchend', function (e) {
+                e.preventDefault();
+
+                moveStick(stick, { left: centerPosition.left - settings.stickRadius,
+                    top: centerPosition.top - settings.stickRadius
+                });
+
+                var pos = getStickPosition({ pageX: centerPosition.left, pageY: centerPosition.top });
+                updatePositionLabel(pos);
+                touchId = null;
+
+            });
+
+
+            function writeToLog(message) {
+                //var $log = $('#Log');
+                //$('<div>', { innerText : message }).appendTo($log);
+            }
+
+            function createStick(container, center, radius) {
+
+                var $container = $(container);
+
+                // We're currently assuming that the thumbStick will go into the centre of the element.
+                var left = center.left - radius;
+                var top = center.top - radius;
+
+                var _stick = $('<div>', {
+                    id: container.id + '_Stick',
+                    style: 'opacity: 0.25; background-color: #0099cc; border: 5px solid #0099cc; position: absolute; left: ' + left + 'px; top: ' + top + 'px; border-radius:' + (settings.stickRadius + 5) + 'px;',
+                    width: (settings.stickRadius * 2) + 'px',
+                    height: (settings.stickRadius * 2) + 'px'
+                });
+
+                _stick.appendTo($container);
+                return _stick;
+            };
+
+
+            function destroyStick(container) {
+
+            };
+
+            function createRange(container, center, radius) {
+
+                var $container = $(container);
+
+                // We're currently assuming that the thumbStick will go into the centre of the element.
+                var left = center.left - radius;
+                var top = center.top - radius;
+
+                $('<div>', {
+                    id: container.id + '_Range',
+                    style: 'opacity: 0.4; border: 3px solid #0099cc; position: absolute; left: ' + left + 'px; top: ' + top + 'px; border-radius:' + (radius+3) + 'px;',
+                    width: (radius * 2) + 'px',
+                    height: (radius * 2) + 'px'
+                }).appendTo($container);
+
+                updatePositionLabel({
+                    axes: { xAxis: 0, yAxis: 0 },
+                    vector: { direction: 0, magnitude: 0 },
+                    pageCoordinates: { left: 0, top: 0 }
+                });
+
+            };
+
+
+            function destroyRange() {
+            };
+
+
+            function updatePositionLabel(pos)
+            {
+                settings.outputLabel.html('\
+                    <div>xAxis: ' + pos.axes.xAxis + '</div>\
+                    <div>yAxis: ' + pos.axes.yAxis + '</div>\
+                    <div>Direction: ' + pos.vector.direction + '</div>\
+                    <div>Magnitude: ' + pos.vector.magnitude + '</div>\
+                    <div>Left: ' + pos.pageCoordinates.left + '</div>\
+                    <div>Top: ' + pos.pageCoordinates.top + '</div>');
+
+                //settings.gameController.setValue($this.id, 'direction', pos.vector.direction);
+                //settings.gameController.setValue($this.id, 'magnitude', pos.vector.magnitude);
+
+                updateServer($this.id, pos.axes.xAxis, pos.axes.yAxis, pos.vector.direction, pos.vector.magnitude)
+
+            }
+
+            function updateServer(id, xAxis, yAxis, direction, magnitude) 
+            {
+                settings.gameController.setThumbStick(id, xAxis, yAxis, direction, magnitude);
+                settings.gameController.updateServer();
+            }
+
+
+
+
+
+            // Paramters:
+            // touch: accepts the touch event that indicates the requested location of the joystick.
+
+            // Returns an object with the following:
+            // axes: the X/Y coordinate values relative to the centre (+32768 to 32768) as xAxis and yAxis
+            // vector: returns the  direction in degrees (0-359) as direction, and the magnitude (0 to +32768) of the joystick as magnitude
+            // pageCoordinates: returns the top left position of the joystick as it should be rendered on the page.
+
+            function getStickPosition(touch) {
+
+
+                // Limit the movement of our thumbstick so that it stays within the range.
+                var movementRadius = (settings.maxRadius - settings.stickRadius);
+
+                
+
+                // Convert this to scale to -32768 to +32768
+                // 32768 was chosen as that's the hid value for the Physical xbox controller in windows.  It may be part of the specification
+                // but as this has been written to provide an alternate method of input for that directlym i'm sticking with it.
+
+                var xAxis = convertRange((touch.pageX - centerPosition.left),0-movementRadius, movementRadius, -32768, +32768);
+                var yAxis = convertRange((0 - (touch.pageY - centerPosition.top)), 0 - movementRadius, movementRadius, -32768, +32768);
+
+
+
+                // Convert the magnitude to 0 to 32768
+                var vectorValue = { direction: Math.round(convertCoordinatestoDirection(xAxis, yAxis)), magnitude: Math.round(convertCoordianesToMagnitude(xAxis, yAxis)) };
+
+
+
+                if (xAxis > 32768) { xAxis = 32768 };
+                if (xAxis < -32768) { xAxis = -32768 };
+                if (yAxis > 32768) { yAxis = 32768 };
+                if (yAxis < -32768) { yAxis = -32768 };
+
+
+                var axesValue = {
+                    xAxis: Math.round(xAxis),
+                    yAxis: Math.round(yAxis),
+                };
+                
+
+
+
+
+
+                // Page coordinates
+                var coords = getXY(vectorValue)
+
+                /*
+                var pageCoordinatesValue = {
+                    left: touch.pageX - settings.stickRadius,
+                    top: touch.pageY - settings.stickRadius
+                };
+                */
+                var pageCoordinatesValue = {
+                    left: Math.round(coords.xAxis+(centerPosition.left-settings.maxRadius)),
+                    top: Math.round(coords.yAxis + (centerPosition.top-settings.maxRadius))
+                };
+
+
+                var retVal = {
+                    axes: axesValue,
+                    vector: vectorValue,
+                    pageCoordinates: pageCoordinatesValue
+                };
+
+                return retVal;
+            }
+
+
+
+
+
+            function convertCoordinatestoDirection(xAxis, yAxis) {
+                // 0 is considered the origin point
+                // The range is an integer xAxis and yAxis can deviate from 0
+
+                var radians = Math.atan2(xAxis, yAxis);
+                var orientation = (radians * (180 / Math.PI));
+
+                orientation = 0 - orientation;
+
+                // make sure any 360+ angles are pushed back down into range.
+                var direction = (360 - orientation) % 360;
+
+                return direction;
+
+            }
+
+
+            function convertCoordianesToMagnitude(xAxis, yAxis) {
+
+                var MaxRange = 32768;
+                var DeadzoneTolerance = 4000;
+                var OuterDeadzone = 350;
+
+                var magnitude = Math.sqrt(Math.pow(xAxis, 2) + Math.pow(yAxis, 2));
+
+                if (magnitude < DeadzoneTolerance)
+                {
+                    magnitude = 0;
+                }
+                else
+                {
+                    // Scale so deadzone is removed, and max value is MaxRange
+                    magnitude = ((magnitude - DeadzoneTolerance) / (32768 - DeadzoneTolerance)) * (MaxRange + OuterDeadzone);
+                    if (magnitude > MaxRange)
+                    {
+                        magnitude = MaxRange;
+                    }
+                }
+
+                return magnitude;
+
+
+            }
+
+
+            function getXY(vector)
+            {
+                var radians = DegreesToRadians(vector.direction);
+
+                // Scale the magnitude
+                var maxrange = (settings.maxRadius-settings.stickRadius)-2;
+                var magnitude = (vector.magnitude / 32768) * (maxrange);
+
+
+                // Opposite Side
+                var sin = Math.sin(radians);
+
+
+
+                //double opposite = sin * magnitude;
+                var xAxisval = maxrange + (sin * magnitude);
+
+                // Adjacent Side
+                var cos = Math.cos(radians);
+                //double adjacent = cos * magnitude;
+                var yAxisval = maxrange - (cos * magnitude);
+
+                return { xAxis: xAxisval, yAxis: yAxisval };
+            }
+
+
+            function DegreesToRadians(degrees)
+            {
+                return degrees * (Math.PI / 180.0);
+            }
+
+            function convertRange(value, oldRangeMin, oldRangeMax, newRangeMin, NewRangeMax) {
+                // Credit to:
+                // http://stackoverflow.com/questions/929103/convert-a-number-range-to-another-range-maintaining-ratio
+                return ((value - oldRangeMin) / (oldRangeMax - oldRangeMin)) * (NewRangeMax - newRangeMin) + newRangeMin
+            }
+
+
+
+
+
+
+            function moveStick(elem, axes) {
+                elem.css({ left: axes.left, top: axes.top });
+            }
+
+
+
+        });
+    };
+
+}(jQuery));
